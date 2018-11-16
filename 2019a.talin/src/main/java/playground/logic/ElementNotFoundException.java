b1404625545c72c4d6b7ee435c00ea50f5b3d044
itaii <itaii@DESKTOP-1QCMHO4.mynet>
2018-11-16 12:02:54 +0200

    added classes ElementNotFoundException,UserNotFoundException.

---
+++ 2019a.talin/src/main/java/playground/logic/ElementNotFoundException.java
@@ -0,0 +1,23 @@
+package playground.logic;
+
+public class ElementNotFoundException extends Exception{
+	private static final long serialVersionUID = -4402147946901277231L;
+
+	public ElementNotFoundException() {
+		super();
+	}
+
+	public ElementNotFoundException(String message) {
+		super(message);
+	}
+
+	public ElementNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ElementNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+}
+
