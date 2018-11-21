2289afb4d16e10d689956b565cd9a7d0d2be67b2
Sapir <Sapir@Sapir-Laptop>
2018-11-21 19:15:00 +0200

    Fix for the elementsDatabase in PlaygroundServiceStub

--- 2019a.talin/src/main/java/playground/logic/Services/PlaygroundServiceStub.java
+++ 2019a.talin/src/main/java/playground/logic/Services/PlaygroundServiceStub.java
@@ -1,6 +1,5 @@
 package playground.logic.Services;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -21,62 +20,62 @@ import playground.logic.Entities.ElementEntity;
 import playground.logic.Exceptions.ElementNotFoundException;
 
 @Service
-public class PlaygroundServiceStub implements PlaygroundService{
-	//private Map<String, UserEntity> usersDatabase;
+public class PlaygroundServiceStub implements PlaygroundService {
+	// private Map<String, UserEntity> usersDatabase;
 	private Map<String, ElementEntity> elementsDatabase;
 	private Map<String, ActivityEntity> activitiesDatabase;
-	
+
 	@PostConstruct
 	public void init() {
-		//this.usersDatabase = new HashMap<>();
-		//setElementsDatabase(elementsDatabase);
+		// this.usersDatabase = new HashMap<>();
+		this.elementsDatabase = new HashMap<>();
 		this.activitiesDatabase = new HashMap<>();
 	}
-	
+
 	public void setElementsDatabase(Map<String, ElementEntity> elementsDatabase) {
 		Date exirationDate = null;
 		String type = "animal";
 		String creatorPlayground = "2019a.talin";
 		String creatorEmail = "2019a.Talin@Gmail.com";
-		Map<String,Object> attributes = new HashMap<>();
-		
-		//add specific attribute
+		Map<String, Object> attributes = new HashMap<>();
+
+		// add specific attribute
 		Random rand = new Random();
 		if (rand.nextInt(100) < 20) { // 20% of the elements
-		    attributes.put("Eat", "meat");
+			attributes.put("Eat", "meat");
 		}
-				
-		//location,value,exirationDate,type,attributes,creatorPlayground,creatorEmail
+
+		// location,value,exirationDate,type,attributes,creatorPlayground,creatorEmail
 		this.elementsDatabase = IntStream.range(0, 100) // int stream
-				.mapToObj(value -> new ElementEntity(value+"",new Location(value,value),"animal #" + value,exirationDate,type,attributes,creatorPlayground,creatorEmail)) //  ElementTO stream using constructor reference
+				.mapToObj(value -> new ElementEntity(value + "", new Location(value, value), "animal #" + value,
+						exirationDate, type, attributes, creatorPlayground, creatorEmail)) // ElementTO stream using
+																							// constructor reference
 				.collect(Collectors.toMap(ElementEntity::getId, Function.identity()));
 	}
 
-
-
 	@Override
 	public synchronized void addNewElement(ElementEntity elementEntity) {
-		this.elementsDatabase.put(elementEntity.getPlayground()+elementEntity.getId(), elementEntity);
+		this.elementsDatabase.put(elementEntity.getPlayground() + elementEntity.getId(), elementEntity);
 	}
 
 	@Override
 	public void addNewActivity(ActivityEntity activityEntity) {
 		this.activitiesDatabase.put(activityEntity.getId(), activityEntity);
-		
+
 	}
 
 	@Override
-	public ElementEntity getElement(String element_id,String element_Playground) throws ElementNotFoundException {
-		ElementEntity rv =  this.elementsDatabase.get(element_Playground+element_id);
+	public ElementEntity getElement(String element_id, String element_Playground) throws ElementNotFoundException {
+		ElementEntity rv = this.elementsDatabase.get(element_Playground + element_id);
 		if (rv == null) {
-			throw new ElementNotFoundException("could not find element by id: " + element_Playground+element_id);
+			throw new ElementNotFoundException("could not find element by id: " + element_Playground + element_id);
 		}
 		return rv;
 	}
-	
+
 	@Override
 	public ActivityEntity getActivity(String activity_id) throws Exception {
-		ActivityEntity rv =  this.activitiesDatabase.get(activity_id);
+		ActivityEntity rv = this.activitiesDatabase.get(activity_id);
 		if (rv == null) {
 			throw new RuntimeException("could not find activity by id: " + activity_id);
 		}
@@ -87,42 +86,38 @@ public class PlaygroundServiceStub implements PlaygroundService{
 	public void cleanup() {
 		this.elementsDatabase.clear();
 		this.activitiesDatabase.clear();
-		//this.usersDatabase.clear();	
+		// this.usersDatabase.clear();
 	}
 
 	@Override
 	public synchronized List<ElementEntity> getAllElements(int size, int page) {
-		return this.elementsDatabase
-				.values() // collection of entities
+		return this.elementsDatabase.values() // collection of entities
 				.stream() // stream of entities
-				.skip(size*page)
-				.limit(size)
-				.collect(Collectors.toList());
+				.skip(size * page).limit(size).collect(Collectors.toList());
 	}
 
 	@Override
 	public boolean validateActivityType(String type) {
 		boolean result;
-				
-		switch(type)
-		{
+
+		switch (type) {
 		case "Play":
 			result = true;
-			break;	
-			
+			break;
+
 		case "Place an add":
 			result = true;
 			break;
-			
+
 		case "Read an add":
 			result = true;
 			break;
-			
+
 		default:
 			result = false;
 			break;
 		}
-		
+
 		return result;
 	}
 
@@ -137,58 +132,48 @@ public class PlaygroundServiceStub implements PlaygroundService{
 	public Map<String, ElementEntity> getElementsDatabase() {
 		return elementsDatabase;
 	}
-	
-	public synchronized List<ElementEntity> getAllNearElements(double x,double y, double distance,int size,int page)
-	{
-		return
-				this.elementsDatabase.values()
-				.stream() // stream of entities
-				.filter(ent-> Math.abs(ent.getLocation().getX()-x) < distance)
-				.filter(ent-> Math.abs(ent.getLocation().getY()-y) < distance)
-				.skip(size*page)
-				.limit(size)
-				.collect(Collectors.toList());		
+
+	public synchronized List<ElementEntity> getAllNearElements(double x, double y, double distance, int size,
+			int page) {
+		return this.elementsDatabase.values().stream() // stream of entities
+				.filter(ent -> Math.abs(ent.getLocation().getX() - x) < distance)
+				.filter(ent -> Math.abs(ent.getLocation().getY() - y) < distance).skip(size * page).limit(size)
+				.collect(Collectors.toList());
 	}
-	
+
 	@Override
-	public synchronized void updateElement(ElementEntity updatedElementEntity, String playground, String id) throws Exception {
-		
-		if(this.elementsDatabase.containsKey(playground+id))
-		{
-			ElementEntity elementEntity = this.elementsDatabase.get(playground+id);
-			
-			if(!elementEntity.getLocation().equals(updatedElementEntity.getLocation()))
-			{
+	public synchronized void updateElement(ElementEntity updatedElementEntity, String playground, String id)
+			throws Exception {
+
+		if (this.elementsDatabase.containsKey(playground + id)) {
+			ElementEntity elementEntity = this.elementsDatabase.get(playground + id);
+
+			if (!elementEntity.getLocation().equals(updatedElementEntity.getLocation())) {
 				elementEntity.setLocation(updatedElementEntity.getLocation());
 			}
-			
-			if(!elementEntity.getName().equals(updatedElementEntity.getName()))
-			{
+
+			if (!elementEntity.getName().equals(updatedElementEntity.getName())) {
 				elementEntity.setName(updatedElementEntity.getName());
 			}
-			
-			if(!elementEntity.getExirationDate().equals(updatedElementEntity.getExirationDate()))
-			{
+
+			if (!elementEntity.getExirationDate().equals(updatedElementEntity.getExirationDate())) {
 				elementEntity.setExirationDate(updatedElementEntity.getExirationDate());
 			}
-			
-			if(!elementEntity.getType().equals(updatedElementEntity.getType()))
-			{
+
+			if (!elementEntity.getType().equals(updatedElementEntity.getType())) {
 				elementEntity.setType(updatedElementEntity.getType());
 			}
-			
-			if(!elementEntity.getAttributes().equals(updatedElementEntity.getAttributes()))
-			{
+
+			if (!elementEntity.getAttributes().equals(updatedElementEntity.getAttributes())) {
 				elementEntity.setAttributes(updatedElementEntity.getAttributes());
 			}
-			
-			this.elementsDatabase.put(playground+id, elementEntity);
-		
-		}
-		else {
+
+			this.elementsDatabase.put(playground + id, elementEntity);
+
+		} else {
 			throw new ElementNotFoundException("Did not found the element");
 		}
-		
+
 	}
 
 }
